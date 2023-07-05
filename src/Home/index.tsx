7ce760cf3c9b4f3ef06b09c3549fb4fe607551db
Merces-dev <giovanitjs013@gmail.com>
2023-07-05 12:44:44 -0300

    feat[rentx]: Initiate rentx project.

---
+++ src/Home/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import { StyleSheet, Text, View } from 'react-native'
+import * as St from "./styles"
+
+const Home = () => {
+    return (
+        <St.Container>
+            <St.Title>home</St.Title>
+        </St.Container>
+    )
+}
+
+export default Home
