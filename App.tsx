498e10ac997149c413f788f1b7e5e30d1303a587
Merces-dev <giovanitjs013@gmail.com>
2023-07-06 01:37:36 -0300

    feat[rentX]: Add global theme.

--- App.tsx
+++ App.tsx
@@ -1,9 +1,32 @@
-import React from 'react';
-import Home from './src/screens/Home';
+import React from "react";
+import Home from "./src/screens/Home";
+
+import {
+  useFonts,
+  Archivo_400Regular,
+  Archivo_500Medium,
+  Archivo_600SemiBold,
+} from "@expo-google-fonts/archivo";
+import { Inter_400Regular, Inter_500Medium } from "@expo-google-fonts/inter";
+import AppLoading from "expo-app-loading";
+import { ThemeProvider } from "styled-components/native";
+import theme from "./src/global/styles/theme";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+    Archivo_400Regular,
+    Archivo_500Medium,
+    Archivo_600SemiBold,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
   return (
-    <Home />
+    <ThemeProvider theme={theme}>
+      <Home />
+    </ThemeProvider>
   );
 }
-
